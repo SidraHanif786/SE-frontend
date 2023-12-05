cc916b0771f27253e282eaad54d82f16bbae89cc
SidraHanif786 <sidrahanif913@gmail.com>
2023-12-06 01:30:37 +0500

    apis integration in products, login, and checkout

--- client/src/components/cart.js
+++ client/src/components/cart.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import { useCart } from './CartContext';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import React from "react";
+import { useCart } from "./CartContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import client from "../service";
+import { useAuth } from "./AuthContext";
 
 const Cart = () => {
+  const { token } = useAuth();
   const { cartItems, removeFromCart, updateQuantity, clearCart } = useCart();
 
   const handleQuantityChange = (productId, newQuantity) => {
@@ -15,13 +18,49 @@ const Cart = () => {
   };
 
   const calculateTotal = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
+  };
+
+  const handleCheckout = async () => {
+    try {
+      // const userToken = localStorage.getItem("userToken");
+      // if (!userToken) {
+      //   console.log("LogIn first");
+      //   return;
+      // }
+
+      const orderData = {
+        products: cartItems.map((item) => ({
+          productId: item._id, // Assuming item.id is the product ID
+          quantity: item.quantity,
+          totalPrice: item.price * item.quantity,
+        })),
+        address: '123 Main St, City, Country', // Hardcoded for now
+        totalBalance: calculateTotal() + 3000,
+      };
+  
+
+      const response = await client.post('/orders/create', orderData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      console.log("Order created successfully:", response.data);
+    } catch (error) {
+      console.error("Error during checkout:", error);
+    }
   };
 
   return (
     <div className="bg-stone-100 min-h-screen pt-[5%]">
       <div className="container mx-auto px-4 pt-20">
-        <h1 className="text-lg font-normal mb-4 md:text-2xl md:font-semibold">Shopping Cart</h1>
+        <h1 className="text-lg font-normal mb-4 md:text-2xl md:font-semibold">
+          Shopping Cart
+        </h1>
         <div className="flex flex-col md:flex-row gap-4">
           <div className="md:w-3/4 mb-4 md:mb-0 overflow-y-auto max-h-[60vh]">
             <div className="bg-white rounded-lg shadow-md p-6">
@@ -30,7 +69,9 @@ const Cart = () => {
                   <tr>
                     <th className="text-left font-semibold">Product</th>
                     <th className="text-left font-semibold">Price</th>
-                    <th className="text-left font-semibold hidden md:flex">Quantity</th>
+                    <th className="text-left font-semibold hidden md:flex">
+                      Quantity
+                    </th>
                     <th className="text-left font-semibold ">Total</th>
                     <th className="text-left font-semibold ">Actions</th>
                   </tr>
@@ -40,29 +81,52 @@ const Cart = () => {
                     <tr key={item.id}>
                       <td className="py-4">
                         <div className="flex items-center">
-                          <img className="h-16 w-16 mr-4" src={item.imageSrc} alt="Product image" />
-                          <span className="font-semibold">{item.name}</span>
+                          <img
+                            className="h-16 w-16 mr-4"
+                            src={item.img[0]}
+                            alt="Product image"
+                          />
+                          <span className="font-semibold">{item.title}</span>
                         </div>
                       </td>
-                      <td className="py-4 ">{`Rs ${item.price}`}</td>
+                      <td className="py-4 ">{`Rs ${item.price || 0}`}</td>
                       <td className="py-4">
                         <div className="flex items-center hidden md:flex">
                           <button
                             className="border rounded-md py-1 px-2 mr-2"
-                            onClick={() => handleQuantityChange(item.id, item.quantity - 1)}
-                          >
+                            onClick={() => {
+                              const newQuantity = item.quantity - 1;
+                              if (newQuantity > 0) {
+                                handleQuantityChange(item.id, newQuantity);
+                              } else {
+                                // If quantity reaches 0, remove the item
+                                handleRemoveItem(item.id);
+                              }
+                            }}
+                            disabled={item.total === 0}>
                             -
                           </button>
-                          <span className="text-center w-8 ">{item.quantity}</span>
+                          <span className="text-center w-8 ">
+                            {item.quantity}
+                          </span>
                           <button
                             className="border rounded-md py-1 px-2 ml-1"
-                            onClick={() => handleQuantityChange(item.id, item.quantity + 1)}
-                          >
+                            onClick={() => {
+                              if (item.total > item.quantity) {
+                                handleQuantityChange(
+                                  item.id,
+                                  item.quantity + 1
+                                );
+                              }
+                            }}
+                            disabled={item.total <= item.quantity}>
                             +
                           </button>
                         </div>
                       </td>
-                      <td className="py-4">{`Rs ${item.price * item.quantity}`}</td>
+                      <td className="py-4">{`Rs ${
+                        item.price * item.quantity
+                      }`}</td>
                       <td className="py-4 ">
                         <FontAwesomeIcon
                           icon={faTrash}
@@ -94,9 +158,15 @@ const Cart = () => {
               <hr className="my-2" />
               <div className="flex justify-between mb-2">
                 <span className="font-semibold">Total</span>
-                <span className="font-semibold">{`Rs ${calculateTotal() + 3000}`}</span>
+                <span className="font-semibold">{`Rs ${
+                  calculateTotal() + 3000
+                }`}</span>
               </div>
-              <button className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full">Checkout</button>
+              <button
+                className="bg-blue-500 text-white py-2 px-4 rounded-lg mt-4 w-full"
+                onClick={handleCheckout}>
+                Checkout
+              </button>
             </div>
           </div>
         </div>
--- client/src/components/products.js
+++ client/src/components/products.js
@@ -1,186 +1,102 @@
-import React from 'react';
-import { useCart } from './CartContext';
+import React, { useEffect, useState } from "react";
+import { useCart } from "./CartContext";
+import client from "../service";
 
+const Products = () => {
+  const { addToCart } = useCart();
+  const [products, setProducts] = useState([]);
+  const [pageInfo, setPageInfo] = useState({
+    totalDocs: 0,
+    totalPages: 1,
+    page: 1,
+    hasPrevPage: false,
+    hasNextPage: false,
+  });
 
-const products = [
-    {
-      id: 1,
-      name: 'Olive Green Sofa Chair',
-      href: '#',
-      priceOriginal: 'Rs 60000',
-      price: '48000',
-      imageSrc: 'F.jpg',
-      imageAlt: 'Olive drab green sofa chair',
-    },
-    {
-      id: 2,
-      name: 'Wooden Chair',
-      href: '#',
-      priceOriginal: 'Rs 50000',
-      price: '35000',
-      imageSrc: 'Chair.jpg',
-      imageAlt: 'Wooden Chair',
-    },
-    {
-      id: 3,
-      name: 'Brown Leather Two Seater',
-      href: '#',
-      priceOriginal: 'Rs 99000',
-      price: '89000',
-      imageSrc: 'two-seater.jpeg',
-      imageAlt: 'Two Seater Sofa',
-    },
-    {
-      id: 4,
-      name: 'Blue Sofa Chair',
-      href: '#',
-      priceOriginal: 'Rs 50000',
-      price: '35000',
-      imageSrc: 'blue.avif',
-      imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-    },
-    {
-        id: 5,
-        name: 'Yellow Sofa Chair',
-        href: '#',
-        priceOriginal: 'Rs 60000',
-        price: '48000',
-        imageSrc: 'yellow.avif',
-        imageAlt: 'Yellow Sofa Chair',
-      },
-      {
-        id: 6,
-        name: 'Pink Sofa Chair',
-        href: '#',
-        priceOriginal: 'Rs 58000',
-        price: '45000',
-        imageSrc: 'pink.jpeg',
-        imageAlt: 'Pink Sofa Chair',
-      },
-      {
-        id: 7,
-        name: 'Wooden Sideboard Table',
-        href: '#',
-        priceOriginal: 'Rs 50000',
-        price: '29000',
-        imageSrc: 'table.avif',
-        imageAlt: 'Wooden Sideboard Table with books',
-      },
-      {
-        id: 8,
-        name: 'Camel Side Table',
-        href: '#',
-        priceOriginal: 'Rs 30000',
-        price: '12000',
-        imageSrc: 'product-8.png',
-        imageAlt: 'Camel sidetable',
-      },
-      {
-        id: 9,
-        name: 'Olive Green Three Seater Sofa',
-        href: '#',
-        priceOriginal: 'Rs 60000',
-        price: '48000',
-        imageSrc: 'olive-green.avif',
-        imageAlt: 'Olive Green Three Seater Sofa',
-      },
-      {
-        id: 10,
-        name: 'Cream Armchair',
-        href: '#',
-        priceOriginal: 'Rs 50000',
-        price: '35000',
-        imageSrc: 'cream.webp',
-        imageAlt: 'Wooden Chair',
-      },
-      {
-        id: 11,
-        name: 'Brown Leather Two Seater',
-        href: '#',
-        priceOriginal: 'Rs 99000',
-        price: '89000',
-        imageSrc: 'two-seater.jpeg',
-        imageAlt: 'Two Seater Sofa',
-      },
-      {
-        id: 12,
-        name: 'Blue Sof Chair',
-        href: '#',
-        priceOriginal: 'Rs 50000',
-        price: '35000',
-        imageSrc: 'blue.avif',
-        imageAlt: 'Hand holding black machined steel mechanical pencil with brass tip and top.',
-      },
-      {
-          id: 13,
-          name: 'Yellow Sofa Chair',
-          href: '#',
-          priceOriginal: 'Rs 70000',
-          price: '48000',
-          imageSrc: 'yellow.avif',
-          imageAlt: 'Yellow Sofa Chair',
-        },
-        {
-          id: 14,
-          name: 'Arm Chair Pillow',
-          href: '#',
-          priceOriginal: 'Rs 55000',
-          price: '45000',
-          imageSrc: 'armchair.avif',
-          imageAlt: 'Arm Chair pillow',
-        },
-        {
-          id: 15,
-          name: 'Wooden Sideboard Table',
-          href: '#',
-          priceOriginal: 'Rs 50000',
-          price: '29000',
-          imageSrc: 'table.avif',
-          imageAlt: 'Wooden Sideboard Table with books',
-        },
-        {
-          id: 16,
-          name: 'Camel Side Table',
-          href: '#',
-          priceOriginal: 'Rs 20000',
-          price: '12000',
-          imageSrc: 'product-8.png',
-          imageAlt: 'Camel sidetable',
-        },
-    
-    
-  ]
-  
-  const Products = () => {
-    const { addToCart } = useCart();
-  
-    const handleAddToCart = (product) => {
-      addToCart(product);
-    };
-  
-    return (
-      <div className="bg-stone-100 pt-20">
-        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 className="font-normal text-xl text-center pb-8 md:text-3xl">All Products</h2>
-  
-          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
-            {products.map((product) => (
-              <a key={product.id} href={product.href} className="group relative">
-                <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-                  <img src={product.imageSrc} alt={product.imageAlt} />
-                  <div className="absolute h-full w-full object-cover object-center group-hover:opacity-100 flex items-center justify-center -bottom-10 group-hover:bottom-0 transition-all duration-300 bg-black/20 opacity-0">
-                    <button className="bg-teal-700 rounded-lg text-white py-2 px-5 hover:bg-teal-900 transition-all duration-300" onClick={() => handleAddToCart(product)}>Add to Cart</button>
-                  </div>
+  useEffect(() => {
+    fetchProducts(1, 12);
+  }, []); // Fetch products on component mount
+
+  const fetchProducts = async (page = 1, pageSize = 12) => {
+    try {
+      const response = await client.get(
+        `/product/getAll?page=${page}&pageSize=${pageSize}`
+      );
+      const responseData = response.data.data;
+      setProducts(responseData.docs);
+
+      setPageInfo({
+        totalDocs: responseData.totalDocs,
+        totalPages: responseData.totalPages,
+        page: responseData.page,
+        hasPrevPage: responseData.hasPrevPage,
+        hasNextPage: responseData.hasNextPage,
+      });
+    } catch (error) {
+      console.error("Error fetching products:", error);
+    }
+  };
+
+  const handleAddToCart = (product) => {
+    addToCart(product);
+  };
+
+  const handlePageChange = async (newPage) => {
+    await fetchProducts(newPage, 12);
+  };
+
+  return (
+    <div className="bg-stone-100 pt-20">
+      <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+        <h2 className="font-normal text-xl text-center pb-8 md:text-3xl">
+          All Products
+        </h2>
+
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          {products?.map((product) => (
+            <a key={product._id} href="#" className="group relative">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+                <img src={product.img[0]} alt={product.title} />
+                <div className="absolute h-full w-full object-cover object-center group-hover:opacity-100 flex items-center justify-center -bottom-10 group-hover:bottom-0 transition-all duration-300 bg-black/20 opacity-0">
+                  {product.total === 0 ? (
+                    <button className="bg-gray-500 rounded-lg text-white py-2 px-5 cursor-not-allowed">
+                      Out of Stock
+                    </button>
+                  ) : (
+                    <button
+                      className="bg-teal-700 rounded-lg text-white py-2 px-5 hover:bg-teal-900 transition-all duration-300"
+                      onClick={() => handleAddToCart(product)}>
+                      Add to Cart
+                    </button>
+                  )}
                 </div>
-                <h3 className="mt-3 text-xl text-gray-700">{product.name}</h3>
-                <del className="text-red-700 text-md">{product.priceOriginal} </del>
-                <p className="mt-2 text-lg font-medium text-gray-900 inline-block">{`Rs ${product.price}`}</p>
-              </a>
-            ))}
-          </div>
+              </div>
+              <h3 className="mt-3 text-xl text-gray-700">{product.title}</h3>
+              <del className="text-red-700 text-md">
+                {`$${parseFloat(product.price) + 150}`}{" "}
+              </del>
+              <p className="ml-2 mt-2 text-lg font-medium text-gray-900 inline-block">{`$${product.price}`}</p>
+            </a>
+          ))}
+        </div>
+
+        <div className="flex justify-center mt-8">
+          <button
+            className="bg-gray-300 px-4 py-2 mr-2 disabled:opacity-50 cursor-pointer"
+            disabled={!pageInfo.hasPrevPage}
+            onClick={() => handlePageChange(pageInfo.page - 1)}>
+            Previous
+          </button>
+          <button
+            className="bg-gray-300 px-4 py-2 ml-2 disabled:opacity-50 cursor-pointer"
+            disabled={!pageInfo.hasNextPage}
+            onClick={() => handlePageChange(pageInfo.page + 1)}>
+            Next
+          </button>
         </div>
       </div>
-    );
-  }
-  
-  export default Products;
+    </div>
+  );
+};
+
+export default Products;
