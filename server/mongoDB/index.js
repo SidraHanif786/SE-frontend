62f49bad493e54c9435b197b73b54c1df8862c8e
SidraHanif786 <sidrahanif913@gmail.com>
2023-12-06 01:30:58 +0500

    apis integration in products, login, and checkout

---
+++ server/mongoDB/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const mongoDB = async()=>{
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`MongoDB Connected `);
+    } catch (error) {
+        console.error(`ERROR in DB Connection: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+module.exports = mongoDB;
